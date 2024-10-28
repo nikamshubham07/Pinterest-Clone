575092ff9556e8f2213a2e2a0c79006438de8236
Shubham Nikam <132605738+nikamshubham07@users.noreply.github.com>
2024-10-28 23:20:48 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  
+   
   const createUser = (e) =>{
     e.preventDefault();
   }
